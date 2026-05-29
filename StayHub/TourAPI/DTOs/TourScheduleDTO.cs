d317c0f0fadee850c89e67f57f78f32fe6e2db90
ThNguien <thanhnguyen1240204@gmail.com>
2026-05-29 22:09:02 +0700

    pull code from dev

--- StayHub/TourAPI/DTOs/TourScheduleDTO.cs
+++ StayHub/TourAPI/DTOs/TourScheduleDTO.cs
@@ -14,14 +14,6 @@ namespace TourAPI.DTOs
 
         public DateTime ReturnDate { get; set; }
 
-        public long Price { get; set; }
-
-        public int MaxCapacity { get; set; }
-
-        public int SoldQuantity { get; set; }
-
-        public int AvailableSeats { get; set; }
-
         public string? Note { get; set; }
 
         public virtual ReadTourBasicDTO? Tour { get; set; }
@@ -30,6 +22,7 @@ namespace TourAPI.DTOs
 
         public virtual ICollection<ReadTourScheduleItineraryDTO>? TourScheduleItineraries { get; set; }
 
+        //public virtual ICollection<ReadTourScheduleTicketDTO>? TourScheduleTickets { get; set; }
     }
 
     public abstract class BaseTourScheduleDTO
@@ -44,38 +37,10 @@ namespace TourAPI.DTOs
         [Required(ErrorMessage = "Return date is required")]
         public DateTime ReturnDate { get; set; }
 
-        [Required(ErrorMessage = "Max capacity is required")]
-        [Range(0, int.MaxValue, ErrorMessage = "AvailableSlots cannot be negative")]
-        public int MaxCapacity { get; set; }
-
-        [Required(ErrorMessage = "Price is required")]
-        [Range(0, long.MaxValue, ErrorMessage = "Price cannot be negative")]
-        public long Price { get; set; }
-
-        [RegularExpression("^(Active|Inactive|Cancelled)$", ErrorMessage = "Status must be Active, Inactive, or Cancelled")]
-        public string? Status { get; set; }
-
         public string? Note { get; set; }
-
-        [Required(ErrorMessage = "Available seats is required")]
-        [Range(0, int.MaxValue, ErrorMessage = "Available seat cannot be negative")]
-        public int AvailableSeats { get; set; }
-
     }
 
     public class CreateTourScheduleDTO : BaseTourScheduleDTO { }
 
     public class UpdateTourScheduleDTO : BaseTourScheduleDTO { }
-
-    public class ReserveScheduleSeatsDTO
-    {
-        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1")]
-        public int Quantity { get; set; }
-    }
-
-    public class ReleaseScheduleSeatsDTO
-    {
-        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1")]
-        public int Quantity { get; set; }
-    }
 }
