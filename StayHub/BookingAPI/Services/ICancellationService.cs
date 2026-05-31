b23667df939dc6d69671a8e750e4b1a7d74349bd
TuongMPCE180644 <tuongmpce180644@fpt.edu.vn>
2026-05-31 23:43:24 +0700

    feat: add logic for uc-44, uc-95, uc-96, uc-97

---
+++ StayHub/BookingAPI/Services/ICancellationService.cs
@@ -0,0 +1,13 @@
+﻿using BookingAPI.DTOs;
+using BookingAPI.Models;
+
+namespace BookingAPI.Services
+{
+    public interface ICancellationService
+    {
+        Task<CancellationRequestDetailDTO> CreateCancellationRequestAsync(int customerId, CreateCancellationRequestDTO dto);
+        Task<IEnumerable<CancellationRequestListDTO>> GetCancellationRequestsAsync(string? status);
+        Task<CancellationRequestDetailDTO> GetCancellationRequestDetailsAsync(int id);
+        Task<CancellationRequestDetailDTO> ProcessCancellationRequestAsync(int id, int processedByUserId, ProcessCancellationDTO dto);
+    }
+}
