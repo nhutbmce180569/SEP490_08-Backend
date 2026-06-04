6642c2a98ca86441abdbc5e7b9530ed024817f27
ThNguien <thanhnguyen1240204@gmail.com>
2026-06-05 01:07:45 +0700

    toi uu code

--- StayHub/TourAPI/Services/IReviewService.cs
+++ StayHub/TourAPI/Services/IReviewService.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.OData.Query;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using TourAPI.DTOs;
+using TourAPI.Models;
 
 namespace TourAPI.Services
 {
@@ -11,6 +13,7 @@ namespace TourAPI.Services
         Task<ReadReviewDTO?> GetMyReviewByTourAsync(int tourId, int customerId);
         Task<IEnumerable<ReadReviewDTO>> GetMyReviewsAsync(int customerId);
         Task<IEnumerable<ReadReviewDTO>> GetReviewsByTourAsync(int tourId, bool includeHidden = false);
+        Task<PagedResult<ReadReviewDTO>> GetReviewsByTourODataAsync(int tourId, ODataQueryOptions<Review> options, bool includeHidden = false);
         Task<ReadReviewReplyDTO> CreateReviewReplyAsync(int staffId, CreateReviewReplyDTO model);
         Task<ReadReviewReplyDTO> UpdateReviewReplyAsync(int replyId, int staffId, UpdateReviewReplyDTO model);
         Task DeleteReviewReplyAsync(int replyId, int staffId);
