06f4f87e274c82c4b348c8a6542735620ec3de17
ThNguien <thanhnguyen1240204@gmail.com>
2026-06-09 01:14:24 +0700

    fix: CheckIn for Staff

--- StayHub/BookingAPI/DTOs/ScheduleCustomerDTO.cs
+++ StayHub/BookingAPI/DTOs/ScheduleCustomerDTO.cs
@@ -10,4 +10,6 @@ public class ScheduleCustomerDTO
     public DateOnly? DateOfBirth { get; set; }
     public string? Gender { get; set; }
     public string? Nationality { get; set; }
+
+    public string? PhoneNumber { get; set; }
 }
--- StayHub/BookingAPI/Services/IContentApiClient.cs
+++ StayHub/BookingAPI/Services/IContentApiClient.cs
@@ -5,5 +5,6 @@ namespace BookingAPI.Services
     public interface IContentApiClient
     {
         Task<List<TicketTypeResponseDTO>> GetActiveTicketTypesAsync();
+        Task<TicketTypeResponseDTO?> GetTicketTypeByIdAsync(int ticketTypeId);
     }
 }
