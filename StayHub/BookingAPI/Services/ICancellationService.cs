9cf51400f292041076146211b2840a23c2adb9c2
TuongMPCE180644 <tuongmpce180644@fpt.edu.vn>
2026-07-20 21:41:33 +0700

    fix: update and fix bugs in users and request cancelation

--- StayHub/BookingAPI/Services/ICancellationService.cs
+++ StayHub/BookingAPI/Services/ICancellationService.cs
@@ -1,4 +1,4 @@
-﻿using BookingAPI.DTOs;
+using BookingAPI.DTOs;
 using BookingAPI.Models;
 
 namespace BookingAPI.Services
@@ -6,7 +6,7 @@ namespace BookingAPI.Services
     public interface ICancellationService
     {
         Task<CancellationRequestDetailDTO> CreateCancellationRequestAsync(int customerId, CreateCancellationRequestDTO dto);
-        Task<PaginationDTO<CancellationRequestListDTO>> GetCancellationRequestsAsync(int operatorId, string? status, int page, int pageSize);
+        Task<PaginationDTO<CancellationRequestListDTO>> GetCancellationRequestsAsync(int operatorId, string? status, string? date, int page, int pageSize);
         Task<CancellationRequestDetailDTO> GetCancellationRequestDetailsAsync(int id, int operatorId);
         Task<CancellationRequestDetailDTO> ProcessCancellationRequestAsync(int id, int operatorId, ProcessCancellationDTO dto);
     }
