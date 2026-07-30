208a87c417957f29aadfde0ef029e6938fba2966
Ly Thi Kieu Thy <kieuthy2004@gmail.com>
2026-07-30 14:24:37 +0700

    Revert "Pending changes exported from your codespace"
    This reverts commit 1688c14ef13fed8b5b39ec2391e1e3f1f7b7ce5c.

--- StayHub/GatewayAPI/Program.cs
+++ StayHub/GatewayAPI/Program.cs
@@ -21,7 +21,7 @@ namespace GatewayAPI
             {
                 options.AddPolicy("AllowFrontend", policy =>
                 {
-                    policy.SetIsOriginAllowed(origin => true) 
+                    policy.WithOrigins("http://localhost:5173", "http://192.168.1.58:5173") 
                           .AllowAnyMethod()
                           .AllowAnyHeader()
                           .AllowCredentials();
